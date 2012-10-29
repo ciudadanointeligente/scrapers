2998f713886aa4e69e24d1ead246943bd8837186
Felipe Álvarez <luisfelipealvarez@gmail.com>
2012-10-29 17:50:25 -0300

    Using it for modifiying the municipales project

--- servel_scrapable_classes.rb
+++ servel_scrapable_classes.rb
@@ -2,125 +2,52 @@
 require './scrapable_classes'
 require 'active_record'
 
-class Voter < ActiveRecord::Base
+ActiveRecord::Base.pluralize_table_names = false
+
+class Municipales2012_comuna < ActiveRecord::Base
+	#cambiar el plural de municipales2012?_comuna
 end
 
 class ServelDB < StorageableInfo
 
 	def initialize(max, min)
-                super()
-                @location = 'http://consulta.servel.cl'
-		# Cambiar!!!
-		@params = 'btnconsulta=SUBMIT&__ASYNCPOST=true&__EVENTARGUMENT=&__EVENTTARGET=&__EVENTVALIDATION=%2FwEWAwL9iqj%2BBQK7%2B6rmDAKVq8qQCQ%3D%3D&__VIEWSTATE=%2FwEPDwUJNzUwMjI5OTkzD2QWAgIDD2QWAgIDDw8WBB4EVGV4dGUeB0VuYWJsZWRoZGRk&hdfl=&txtRUN='
-		@param_names = {
-				'btn' => 'btnconsulta',
-				'event' => '__EVENTVALIDATION',
-				'view' => '__VIEWSTATE',
-				'rut' => 'txtRUN'
-				}
-		@ruts = Integer(max).downto(Integer(min))
-		@file_name = 'servel_'+max.to_s+'-'+min.to_s
-		#Set xpaths
-		@xpath_view = '//*[@id="__VIEWSTATE"]'
-		@xpath_event = '//*[@id="__EVENTVALIDATION"]'
-		@xpath_rut = '//*[@id="lbl_run"]'
-		@xpath_name = '//*[@id="lbl_nombre"]/text()'
-		@xpath_gender = '//*[@id="lbl_sexo"]'
-		@xpath_electoral_address = '//*[@id="lbl_domelect"]/text()'
-		@xpath_circunscriptional_address = '//*[@id="lbl_cirelect"]'
-		@xpath_commune = '//*[@id="lbl_comuna"]'
-		@xpath_province = '//*[@id="lbl_provincia"]'
-		@xpath_region = '//*[@id="lbl_region"]'
-		@xpath_table = '//*[@id="lbl_mesa"]'
-		@xpath_voting_place = '//*[@id="lbl_localv"]'
-		@xpath_voting_place_address = '//*[@id="lbl_direcvocal"]'
-		@xpath_vocal_condition = '//*[@id="lbl_codvocal"]/text()'
-		@xpath_scrutineer_condition = '//*[@id="lbl_codcolegio"]/text()'
-		#Get initial keys
-		data = open(@location).read
-		html = Nokogiri::HTML(data, nil, 'utf-9')
-		@param_values = {
-				'btn' => 'SUBMIT',
-				'view' => CGI::escape(html.xpath(@xpath_view).first['value']),
-				'event' => CGI::escape(html.xpath(@xpath_event).first['value']),
-				'rut' => ''
-				}
-                ActiveRecord::Base.establish_connection(
-                        :adapter => "mysql",
-                        :host => "localhost",
-                        :database => "servel",
-			:username => "root",
-			:password => "pass"
+        super()
+        @location = 'http://www.elecciones.gov.cl/mobile/alcaldesMobile.action'
+        ActiveRecord::Base.establish_connection(
+            :adapter => "mysql",
+            :host => "localhost",
+            :database => "asdasdasd",
+			:username => "asdasd",
+			:password => "asdasdasdasd"
                 )
 	end
 
-	def params_url rut
-		@param_values['rut'] = rut
-		params = @param_names.merge(@param_values){|key, name, value| [name, value].join('=')}
-		params.values.join('&')
-	end
-
-	def cookies_url
-		response = RestClient.get @location
-		{:cookies => response.cookies}
-	end
-
-	def update_params view, event
-		@param_values['view'] = view
-		@param_values['event'] = event
-	end
-		
-
-	# doc_locations doesn't have the method each
-	def process
-		doc_locations do |doc_location|
-			begin
-				#doc = read doc_location
-				info = get_info doc_location
-				formatted_info = format info
-				save formatted_info
-			rescue Exception=>e
-				p e
-			end
-		end
-	end
-
-	def doc_locations
-		for rut in @ruts
-			p rut
-			yield rut.to_s+verificador(rut)
-		end
-	end
-
 	def format info
-		info
+		formatted_info = {}
+		info.each do |nombre, codigos|
+			codigo_region = codigos[0]
+			codigo_comuna = codigos[1]
+			formatted_info[nombre] = 'codigoRegion='+codigo_region+'&codigoComuna='+codigo_comuna
+		end
+		formatted_info
 	end
 
-	def get_info rut
-		params = params_url rut
-		cookies = cookies_url
-		data = RestClient.post @location, params, cookies
-		html = Nokogiri::HTML(data, nil, 'utf-8')
-		#update keys
-		view = CGI::escape(html.xpath(@xpath_view).first['value'])
-		event = CGI::escape(html.xpath(@xpath_event).first['value'])
-		update_params view, event
+	def get_info doc
+		html = Nokogiri::HTML(doc, nil, 'utf-8')
 		#get data with xpath
-		parsed_data = {
-			'rut' => html.xpath(@xpath_rut).first.children.text.strip,
-			'name' => html.xpath(@xpath_name).first.text.strip,
-			'gender' => html.xpath(@xpath_gender).first.children.text.strip,
-			'electoral_address' => html.xpath(@xpath_electoral_address).first.text.strip,
-			'circunscriptional_address' => html.xpath(@xpath_circunscriptional_address).first.children.text.strip,
-			'commune' => html.xpath(@xpath_commune).first.children.text.strip,
-			'province' => html.xpath(@xpath_province).first.children.text.strip,
-			'region' => html.xpath(@xpath_region).first.children.text.strip,
-			'voting_table' => html.xpath(@xpath_table).first.children.text.strip,
-			'voting_place' => html.xpath(@xpath_voting_place).first.children.text.strip,
-			'voting_place_address' => html.xpath(@xpath_voting_place_address).first.children.text.strip,
-			'vocal_condition' => html.xpath(@xpath_vocal_condition).first.text.strip,
-			'scrutineer_condition' => html.xpath(@xpath_scrutineer_condition).first.text.strip
-		}
+		parsed_data = {}
+		comunas = html.css('ul[@data-role] li')
+		#"div.ui-btn-text a.ui-link-inherit"
+		comunas.each do |comuna|
+			regexp_region = /codigoRegion.value = (\d+);/
+			regexp_comuna = /codigoComuna.value = (\d+);/
+			onclick = comuna.children[0]['onclick']
+			codigo_region = onclick.scan(regexp_region)[0][0]
+			codigo_comuna = onclick.scan(regexp_comuna)[0][0]
+			nombre_comuna = comuna.text.strip
+			parsed_data[nombre_comuna] = [codigo_region, codigo_comuna]
+		end
+		parsed_data
 	end
 
 	def save info
@@ -130,42 +57,7 @@ class ServelDB < StorageableInfo
 		#	file.write("/n")
 		#	file.close()
 		#end
-		#p info
-		Voter.create(
-                        :rut => info['rut'],
-                        :name => info['name'],
-                        :gender => info['gender'],
-                        :electoral_address => info['electoral_address'],
-                        :circunscriptional_address => info['circunscriptional_address'],
-                        :commune => info['commune'],
-                        :province => info['province'],
-                        :region => info['region'],
-                        :voting_table => info['voting_table'],
-                        :voting_place => info['voting_place'],
-                        :voting_place_address => info['voting_place_address'],
-                        :vocal_condition => info['vocal_condition'],
-                        :scrutineer_condition => info['scrutineer_condition'],
-                )
-	end
-
-	def verificador t
-	        v=1
-	        s=0
-	        for i in (2..9)
-	                if i == 8
-	                	v=2
-	                else v+=1
-	        	end
-	        	s+=v*(t%10)
-	        	t/=10
-	        end
-	        s = 11 - s%11
-	        if s == 11
-	        	return 0.to_s
-	        elsif s == 10
-	        	return "K"
-	        else
-	        	return s.to_s
-	        end
+		p info
+		
 	end
 end
