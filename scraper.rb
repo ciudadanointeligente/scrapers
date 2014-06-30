5285e481efb5aa8a2ae0920cb8e1eda28dd915cd
J. Pablo Pérez Trabucco <jpereztrabucco@gmail.com>
2014-06-30 15:58:15 -0400

    changes to work with billit, debug version

--- scraper.rb
+++ scraper.rb
@@ -27,23 +27,15 @@ class GenericStorage
     post record
   end
 
-  def post_to_morph record
-    # Save in morph.io
-    if ((ScraperWiki.select("* from data where `uid`='#{record['uid']}'").empty?) rescue true)
-      ScraperWiki.save_sqlite(['uid'], record)
-      puts "Adds record " + record['uid'] + " for " + record['bill_id']
-    else
-      puts "Skipping already saved record " + record['uid']
-    end
-  end
-
   def post record
-    HTTParty.post(@middleware, {:body => record.to_json, :headers => { 'Content-Type' => 'application/json' } })
+    HTTParty.post(@billit, {:body => record.to_json, :headers => { 'Content-Type' => 'application/json' } })
     puts "Adds record " + record['uid'] + " for " + record['bill_id']
   end
 
   def debug record
     puts '<-----debug-----'
+    puts 'bill: ' + @bill_id
+    puts '----------------'
     p record
     puts '------debug---/>'
   end
@@ -56,8 +48,9 @@ class VotingLowChamber < GenericStorage
     @chamber = 'C.Diputados'
     @location_vote_general = 'http://opendata.camara.cl/wscamaradiputados.asmx/getVotaciones_Boletin?prmBoletin='
     @location_vote_detail = 'http://opendata.camara.cl/wscamaradiputados.asmx/getVotacion_Detalle?prmVotacionID='
-    @middleware = 'http://middleware.congresoabierto.cl/votes'
     @billit_current_location = 'http://billit.ciudadanointeligente.org/bills/search.json?fields=uid&per_page=100'
+    @billit = 'http://billit.ciudadanointeligente.org/bills'
+    @bill_id = String.new
   end
 
   def process
@@ -85,33 +78,39 @@ class VotingLowChamber < GenericStorage
     response = HTTParty.get(@location_vote_detail + voting_id, :content_type => :xml)
     response_votes = response['Votacion']['Votos']['Voto']
     response_pair_up = response['Votacion']['Pareos']['Pareo']
-    
+
     @votes = Array.new
     response_votes.each do |single_vote|
       vote = Hash.new
-      vote['uid'] = single_vote['Diputado']['DIPID']
-      vote['lastname'] = single_vote['Diputado']['Nombre']
-      vote['parental_surname'] = single_vote['Diputado']['Apellido_Paterno']
-      vote['maternal_surname'] = single_vote['Diputado']['Apellido_Materno']
-      vote['option_content'] = single_vote['Opcion']['__content__']
-      vote['option_code'] = single_vote['Opcion']['Codigo']
+      vote['voter_id'] = single_vote['Diputado']['Apellido_Paterno'] + " " + single_vote['Diputado']['Apellido_Materno'] + ", " + single_vote['Diputado']['Nombre']
+      case single_vote['Opcion']['Codigo']
+      when 0 #Negativo
+        vote['option'] = "no"
+      when 1 #Afirmativo
+        vote['option'] = "yes"
+      when 2 #Abstencion
+        vote['option'] = "abstain"
+      else
+        vote['option'] = ""
+      end
       @votes << vote
     end
 
     @pair_ups = Array.new
+    i = 1
     response_pair_up.each do |single_pair_up|
-      pair_up = Hash.new
-      pair_up['congressman1'] = Hash.new
-      pair_up['congressman1']['uid'] = single_pair_up['Diputado1']['DIPID']
-      pair_up['congressman1']['lastname'] = single_pair_up['Diputado1']['Nombre']
-      pair_up['congressman1']['parental_surname'] = single_pair_up['Diputado1']['Apellido_Paterno']
-      pair_up['congressman1']['maternal_surname'] = single_pair_up['Diputado1']['Apellido_Materno']
-      pair_up['congressman2'] = Hash.new
-      pair_up['congressman2']['uid'] = single_pair_up['Diputado2']['DIPID']
-      pair_up['congressman2']['lastname'] = single_pair_up['Diputado2']['Nombre']
-      pair_up['congressman2']['parental_surname'] = single_pair_up['Diputado2']['Apellido_Paterno']
-      pair_up['congressman2']['maternal_surname'] = single_pair_up['Diputado2']['Apellido_Materno']
-      @pair_ups << pair_up
+      # first pair
+      pair_up1 = Hash.new
+      pair_up1['voter_id'] = single_pair_up['Diputado1']['Apellido_Paterno'] + " " + single_pair_up['Diputado1']['Apellido_Materno'] + ", " + single_pair_up['Diputado1']['Nombre']
+      pair_up1['option'] = "paired, set " + i.to_s
+      @pair_ups << pair_up1
+
+      # second pair
+      pair_up2 = Hash.new
+      pair_up2['voter_id'] = single_pair_up['Diputado2']['Apellido_Paterno'] + " " + single_pair_up['Diputado2']['Apellido_Materno'] + ", " + single_pair_up['Diputado2']['Nombre']
+      pair_up2['option'] = "paired, set " + i.to_s
+      @pair_ups << pair_up2
+      i = i + 1
     end
   end
 
@@ -127,100 +126,60 @@ class VotingLowChamber < GenericStorage
         response_voting['Votacion'].each do |voting|
           get_details_of_voting voting['ID']
           record = get_info voting, @votes, @pair_ups
-          post record
-          # debug record  #DEBUG
+          # post record
+          debug record  #DEBUG
         end
       else
         get_details_of_voting response_voting['Votacion']['ID']
         record = get_info response_voting['Votacion'], @votes, @pair_ups
-        post record
-        # debug record  #DEBUG
+        # post record
+        debug record  #DEBUG
       end
     end
   end
 
   def get_info voting, votes, pair_ups
+
+    @bill_id = voting['Boletin']
+
+    vote_events = [
+      {
+        counts = [
+          {
+            'option' => "yes",
+            'value' => voting['TotalAfirmativos'].to_i
+          },
+          {
+            'option' => "no",
+            'value' => voting['TotalNegativos'].to_i
+          },
+          {
+            'option' => "abstain",
+            'value' => voting['TotalAbstenciones'].to_i
+          },
+          {
+            'option' => "paired",
+            'value' => pair_ups.count
+          }
+        ],
+        votes = votes + pair_ups #all votes, from options yes, no and abstain + paired
+      }
+    ]
+
+    # motion
     record = {
-      'uid' => voting['ID'],
-      'chamber' => @chamber,
+      'organization' => @chamber,
+      'text' => if voting['Articulo'].nil? then '' else voting['Articulo'] end,
       'date' => voting['Fecha'],
-      'type_content' => voting['Tipo']['__content__'],
-      'type_code' => voting['Tipo']['Codigo'],
-      'result_content' => voting['Resultado']['__content__'],
-      'result_code' => voting['Resultado']['Codigo'],
-      'quorum_content' => voting['Quorum']['__content__'],
-      'quorum_code' => voting['Quorum']['Codigo'],
-      'session_id' => voting['Sesion']['ID'],
-      'session_number' => voting['Sesion']['Numero'],
-      'session_date' => voting['Sesion']['Fecha'],
-      'session_type_content' => voting['Sesion']['Tipo']['__content__'],
-      'session_type_code' => voting['Sesion']['Tipo']['Codigo'],
-      'bill_id' => voting['Boletin'],
-      'article' => if voting['Articulo'].nil? then '' else voting['Articulo'] end,
-      'procedure_content' => if voting['Tramite'].nil? then '' else voting['Tramite']['__content__'] end,
-      'procedure_code' => if voting['Tramite'].nil? then '' else voting['Tramite']['Codigo'] end,
-      'report_content' => if voting['Informe'].nil? then '' else voting['Informe']['__content__'] end,
-      'report_code' => if voting['Informe'].nil? then '' else voting['Informe']['Codigo'] end,
-      'total_affirmative' => voting['TotalAfirmativos'],
-      'total_negative' => voting['TotalNegativos'],
-      'total_abstentions' => voting['TotalAbstenciones'],
-      'total_dispensed' => voting['TotalDispensados'],
-      'votes' => votes,
-      'pair_ups' => pair_ups,
-      'date_scraped' => Date.today.to_s
+      'requirement' => voting['Quorum']['__content__'],
+      'result' => voting['Resultado']['__content__'],
+      'session' => voting['Sesion']['ID'],
+      'vote_events' => vote_events
     }
-    return record
-  end
-end
 
-class VotingMassStorage < GenericStorage
-  def initialize()
-    super()
-    @middleware = 'http://middleware.congresoabierto.cl/votes.json'
-  end
-
-  def process
-    @response = HTTParty.get(@middleware, :content_type => :json)
-    @response = JSON.parse(@response.body)
-
-    @response.each do |voting|
-      record = get_info voting
-      post_to_morph record
-    end
-  end
-
-  def get_info voting
-    record = {
-      'uid' => voting['uid'],
-      'chamber' => voting['chamber'],
-      'date' => voting['date'],
-      'type_content' => voting['type_content'],
-      'type_code' => voting['type_code'],
-      'result_content' => voting['result_content'],
-      'result_code' => voting['result_code'],
-      'quorum_content' => voting['quorum_content'],
-      'quorum_code' => voting['quorum_code'],
-      'session_id' => voting['session_id'],
-      'session_number' => voting['session_number'],
-      'session_date' => voting['session_date'],
-      'session_type_content' => voting['session_type_content'],
-      'session_type_code' => voting['session_type_code'],
-      'bill_id' => voting['bill_id'],
-      'article' => if voting['article'].nil? then '' else voting['article'].tr("\n","") end,
-      'procedure_content' => if voting['procedure_content'].nil? then '' else voting['procedure_content'] end,
-      'procedure_code' => if voting['procedure_code'].nil? then '' else voting['procedure_code'] end,
-      'report_content' => if voting['report_content'].nil? then '' else voting['report_content'] end,
-      'report_code' => if voting['report_code'].nil? then '' else voting['report_code'] end,
-      'total_affirmative' => voting['total_affirmative'],
-      'total_negative' => voting['total_negative'],
-      'total_abstentions' => voting['total_abstentions'],
-      'total_dispensed' => voting['total_dispensed'],
-      'date_scraped' => Date.today.to_s
-    }
     return record
   end
 end
 
 # Runner
-# VotingMassStorage.new.process # Store the values into a middleware service of ciudadano inteligente
-VotingLowChamber.new.process  # The real scraper, but in morph.io it doesn't work because a memory allocation bug in ruby 1.9.3
+VotingLowChamber.new.process
